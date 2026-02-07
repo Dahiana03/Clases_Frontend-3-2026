9ce24bd703adf322120f92bd78f687a7a223e663
Dahiana <kzapatada@cesde.net>
2026-02-06 20:16:24 -0500

    productcardjsx y productsjs

---
+++ src/data/products.js
@@ -0,0 +1,31 @@
+export const products = [
+    {   
+        id: 1,
+        name: 'Laptop HP PAVILION',
+        price: 4500000,
+        Image: 'https://i5.walmartimages.com/seo/HP-Pavilion-15-6-FHD-Touch-Intel-Core-i5-1135G7-8GB-RAM-512GB-SSD-Lunar-Gold-Windows-11-15-eg0050wm_0f068620-f92c-4184-ab89-8a98297f0b96.73921290232e5458a71a0adc4cd15c8d.jpeg',
+        description: 'Laptop HP PAVILION 15-eh1000la, procesador AMD Ryzen 5 5500U, RAM 8GB, almacenamiento SSD 256GB, pantalla de 15.6" Full HD, Windows 10 Home',
+    },
+    {
+        id: 2,
+        name: 'Mouse Logitech MX Master 3',
+        price: 150000,
+        Image: 'https://lamanzanamordida.net/app/uploads-lamanzanamordida.net/2022/01/MX-Master-3.jpg',
+        description: 'Mouse Logitech MX Master 3, diseño ergonómico, sensor de alta precisión, compatibilidad con múltiples dispositivos',
+    },
+    {
+        id: 3,
+        name: 'Teclado Mecánico Razer BlackWidow',
+        price: 1500000,
+        Image: 'https://assets2.razerzone.com/images/pnx.assets/ffb7aba8befd12f6a29a6f55af5dfadd/razer-blackwidow-v4-x-usp4-chroma-desktop.webp',
+        description: 'Teclado Mecánico Razer BlackWidow, switches mecánicos, retroiluminación RGB personalizable, diseño resistente',
+    },
+    {
+        id: 4,
+        name: 'Monitor LG UltraGear 27GN950',
+        price: 4500000,
+        Image: 'https://www.lg.com/content/dam/channel/wcms/es/images/monitores/27gn950-b_aeu_eees_es_c/27GN950-01.jpg',
+        description: 'Monitor LG UltraGear 27GN950, pantalla de 27" 4K UHD, tasa de refresco de 144Hz, tecnología NVIDIA G-Sync compatible',
+    }
+
+]
